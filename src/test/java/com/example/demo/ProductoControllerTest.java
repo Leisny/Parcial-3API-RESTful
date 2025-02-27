2a0242b631629022b0772b871b1bb5645645fb47
Leisny <leisny.moreno.6445@miremington.edu.co>
2025-02-27 14:20:36 -0500

    cambios

--- src/test/java/com/example/demo/ProductoControllerTest.java
+++ src/test/java/com/example/demo/ProductoControllerTest.java
@@ -1,11 +1,13 @@
 package com.example.demo;
 
+import com.example.demo.Controller.ProductoController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
+
 
 @SpringBootTest
 @AutoConfigureMockMvc
